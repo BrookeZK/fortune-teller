30a291a9fe4b9f7bc4c65376ee4692b78c5ae5f9
Brooke Kullberg and Katlin Anderson <katlin@>
2019-04-02 16:16:23 -0700

    add functionality to form, answers, and deaths

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
   $("form#portents").submit(function(event) {
-
     var boxesChecked = 0;
-    $("#portents").hide();
+    var name = $("input#name").val();
+    $("#answers").prepend("<h1>Hello " + name + ",</h1>")
+    console.log(name);
+    $("#portents, .hidden").hide();
     $("#answers").show();
      $("input:checkbox[name=omens]:checked").each(function(){
       boxesChecked++;
     });
-
     if (boxesChecked <= 2) {
       $("#answer1").show();
     } else if (boxesChecked <= 4) {
@@ -19,4 +20,24 @@ $(document).ready(function(){
     }
     event.preventDefault();
   });
+
+  $("button#death").click(function() {
+    var random = randomNumberGenerator(3, 1);
+    console.log(random);
+    $("#deaths").show();
+    $(".death").hide();
+    if (random === 1) {
+      $("#volcano").show();
+    } else if (random === 2) {
+      $("#geckos").show();
+    } else if (random === 3) {
+      $("#brain-explosion").show();
+    } else {
+      console.log ("randomNumberGenerator function does not work")
+    }
+  });
 });
+
+function randomNumberGenerator(maxNum, minNum) {
+  return (Math.round(Math.random() * (maxNum - minNum)) + minNum);
+};
